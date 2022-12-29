a615dfbf807c08043fe226e613b9d7f132752c5f
Kaustubh <kumar@kaustubh.com>
2022-12-29 13:54:25 +0530

    Commit blog and article over

--- app/src/main/java/com/example/menu/fragments/BlogsFragment.java
+++ app/src/main/java/com/example/menu/fragments/BlogsFragment.java
@@ -1,37 +1,85 @@
 package com.example.menu.fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.menu.ArticleAdapter;
+import com.example.menu.ArticleModel;
+import com.example.menu.BlogAdapter;
+import com.example.menu.BlogModel;
 import com.example.menu.R;
+import com.example.menu.databinding.FragmentGalleryBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the  factory method to
- * create an instance of this fragment.
- */
+
 public class BlogsFragment extends Fragment {
 
+    RecyclerView blog_rec;
+    ArrayList<BlogModel> bloglist;
+    BlogAdapter blogAdapter;
+    FirebaseFirestore db;
 
-    public BlogsFragment() {
-        // Required empty public constructor
+
+    private FragmentGalleryBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+
+
+
+        View root = inflater.inflate(R.layout.activity_blogs_screen, container, false);
+
+        blog_rec= root.findViewById(R.id.blog_rec);
+
+        db = FirebaseFirestore.getInstance();
+
+
+        blog_rec.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.VERTICAL, false));
+        bloglist = new ArrayList<>();
+        //   bloglist.add(new BlogModel("https://firebasestorage.googleapis.com/v0/b/menu-c7a13.appspot.com/o/testimg.png?alt=media&token=49d93405-f40f-45d9-b76d-9673dea47f2a","25m","RO","2-3-22","The description is here"));
+        blogAdapter = new BlogAdapter(getActivity(), bloglist);
+        blog_rec.setAdapter(blogAdapter);
+
+        db.collection("BlogCollection")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @SuppressLint("NotifyDataSetChanged")
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for (QueryDocumentSnapshot documentSnapshot:task.getResult()){
+                                BlogModel blogModel = documentSnapshot.toObject(BlogModel.class);
+                                bloglist.add(blogModel);
+                                blogAdapter.notifyDataSetChanged();
+                            }
+                        }
+                    }
+                });
+
+        return root;
     }
-
-
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blogs, container, false);
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
-
